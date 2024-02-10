973b9b95d7b32c59a5625ee21d58293e39b774a4
macbook <macbook@macbooks-MacBook-Pro.local>
2024-02-10 11:49:41 +0700

    frist commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,40 @@
+/* eslint-disable @next/next/no-css-tags */
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 
 export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
-        <Head></Head>
+        <Head>
+          <link rel="stylesheet" href="/css/bootstrap.css" type="text/css" />
+          <link rel="stylesheet" href="/css/animate.css" type="text/css" />
+          <link rel="stylesheet" href="/css/owl.carousel.css" type="text/css" />
+          <link
+            rel="stylesheet"
+            href="/css/magnific-popup.css"
+            type="text/css"
+          />
+          <link
+            rel="stylesheet"
+            href="/css/jquery.countdown.css"
+            type="text/css"
+          />
+          <link rel="stylesheet" href="/css/style.css" type="text/css" />
+          <link
+            rel="stylesheet"
+            href="/css/animsition.min.css"
+            type="text/css"
+          />
+
+          <link rel="stylesheet" href="/css/bg.css" type="text/css" />
+
+          <link
+            rel="stylesheet"
+            href="/css/color.css"
+            type="text/css"
+            id="colors"
+          ></link>
+        </Head>
         <body>
           <Main />
           <NextScript />
